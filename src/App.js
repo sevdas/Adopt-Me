ea102d3d658fb32fc562a065c51841d655e8051f
sevdas <sshukrieva@gmail.com>
2022-04-04 22:49:33 +0100

    add funtion using useState hook to manage the state of the location input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
+      <SearchParams />
       <Pet name="Luna" animal="Dog" breed="Big-Schnauzer" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
       <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
